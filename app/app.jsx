2a2da66eeeae35866975967d458bfd2802d81cb4
MichaelWiss <michael.wiss@gmail.com>
2017-01-11 12:52:51 -0600

    install router

--- app/app.jsx
+++ app/app.jsx
@@ -1,5 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
 
 ReactDOM.render(
   <h1>Boilerplate app!</h1>,

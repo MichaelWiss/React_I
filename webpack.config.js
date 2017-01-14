70c5dc19bf030d91fd1b873a25fdddcc7aec1ec6
MichaelWiss <michael.wiss@gmail.com>
2017-01-14 14:40:03 -0600

    register jquery in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,21 @@
+var webpack = require('webpack');
+
+
 module.exports = {
-  entry: './app/app.jsx',
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/foundation.mon.js',
+    './app/app.jsx'
+  ],
+  externals: {
+      jquery: 'jquery.min.js'
+  },
+  plugins: [
+      new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+      }) 
+  ],
   output: {
     path: __dirname,
     filename: './public/bundle.js'

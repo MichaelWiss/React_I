29f193ea4cabf5e926701e81c60b6f0a871a4a6d
MichaelWiss <michael.wiss@gmail.com>
2017-01-15 11:05:28 -0600

    buttons

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -11,8 +11,16 @@ var React = require('react');
 var About = (props) => {
 	return (
 	  <div>
-		<h3>About Component</h3>
+		<h1 className="text-center">About</h1>
 		 <p>Welcome to About page</p>
+		 <p>
+		   Made with React
+		 </p>
+		 <ul>
+		   <li>
+		   <a href="https://facebook.github.io">React</a>
+		   </li>
+		 </ul>
      </div>
 	)
 };
